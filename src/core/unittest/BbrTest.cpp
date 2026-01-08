eacdcb5c6ee847b6b6317dc377a8995661b66cf0
Agency <saikatc@microsoft.com>
2026-01-08 11:05:55 -0800

    Initial for BBr

---
+++ src/core/unittest/BbrTest.cpp
@@ -0,0 +1,15 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    Unit tests for Bottleneck Bandwidth and RTT (BBR) congestion control.
+
+--*/
+
+#include "main.h"
+#ifdef QUIC_CLOG
+#include "BbrTest.cpp.clog.h"
+#endif
